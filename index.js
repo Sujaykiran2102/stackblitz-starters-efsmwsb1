77fd430546f40958808ba084bf5bf53debef17ac
unknown <rssujaykiran@gmail.com>
2025-02-13 10:29:04 +0530

    updated

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const { resolve } = require('path');
+require("dotenv").config();
+const mongoose=require('mongoose');
 
 const app = express();
-const port = 3010;
+const PORT = process.env.PORT || 3010;
+const uri = process.env.MONGOD_URI;
 
 app.use(express.static('static'));
+app.use(express.json());
+
+mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{console.log(` Connected to database`)})
+.catch((err)=>{ `Error connecting to database: ${err}`})
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
